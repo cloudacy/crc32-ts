2c3769443ec726b0d9f2afce561fc5c5a88c489b
Patrick <p.kuen@cloudacy.com>
2022-10-02 08:54:33 +0200

    refactor!: convert to esm module

--- dist/es6/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="node" />
-export declare let table: number[];
-export default function crc32(buffer: Buffer, unsigned?: boolean, seed?: number): number;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,3 @@
-/// <reference types="node" />
+/// <reference types="node" resolution-mode="require"/>
 export declare let table: number[];
 export default function crc32(buffer: Buffer, unsigned?: boolean, seed?: number): number;
